edf8a3602c01e046d86c4ef4632f32d88af24674
zahid-soft <zahidsarang95@gmail.com>
2024-07-20 19:09:23 +0530

    arrays in go

---
+++ 8_array/arrays.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// numbered sequence of specific length
+func main() {
+
+	// var nums [4]int
+
+	// array length
+	// fmt.Println(len(nums))
+	// nums[0] = 1
+	// fmt.Println(nums)
+
+	// var vals [4]bool
+	// vals[0] = true
+	// fmt.Println(vals)
+
+	// nums := [3]int{1, 2, 3}
+	// fmt.Println(nums)
+
+	// 2d array
+	nums := [2][2]int{{3, 4}, {5, 6}}
+	fmt.Println(nums)
+
+	// - fixed size, that is predictable
+	// - Memory optimazation
+	// - Constant time access
+}
