1224fe13d483d82ecc247e901cf2f0218da83683
zahid-soft <zahidsarang95@gmail.com>
2024-07-20 18:31:33 +0530

    for loop

---
+++ 5_for_loop/forLoop.go
@@ -0,0 +1,33 @@
+package main
+
+// for => only construct in go for looping
+func main() {
+
+	// while loop using for looping
+
+	// i := 1
+
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i = i + 1
+	// }
+
+	// infinite loop
+	// for {
+	// 	println(1)
+	// }
+
+	// for i := 0; i <= 3; i++ {
+	// 	if i == 2 {
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	// range
+
+	// for i := range 10 + 1 {
+	// 	fmt.Println(i)
+	// }
+
+}
